4837262120075c2458eeb7cb64b210afae167a85
Debarghya Ghoshdastidar <gdebarghya@gmail.com>
2018-09-26 16:48:31 +0200

    Update experiment1c_main.m

--- experiment1c_main.m
+++ experiment1c_main.m
@@ -1,6 +1,11 @@
 function experiment1c_main (trial)
-
+% Runs experiments for the paper
+% Input:
 % trial = number of independent runs (we use trial = 1000)
+%
+% Output (saved in mat file):
+% Test power (percentage of rejections) for different tests
+
 n_all = 50:50:200; % number of vertices
 m_all = [10 20 50 100 200]; % sample size
 bs = 200; % number of bootstrap samples generated
